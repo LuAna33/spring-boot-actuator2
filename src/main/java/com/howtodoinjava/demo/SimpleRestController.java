16d0c81d22d4dbd3ca9d03f10446bd7f5d934131
Lu Ana <luy22@live.com.ar>
2020-11-25 13:25:50 -0300

    Actualizacion SimpleRestController.java

--- src/main/java/com/howtodoinjava/demo/SimpleRestController.java
+++ src/main/java/com/howtodoinjava/demo/SimpleRestController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleRestController {
 	@GetMapping("/example")
 	public String example() {
-		return "Hello User !! " + new Date();
+		return "Hola Usuario !! " + new Date();
 	}
 
 	public static void main(String[] args) {
-		String text = "Lorem Ipsum is simply dummy text";
+		String text = "Texto simulado";
 		String substring = text.substring(3);
 		System.out.println(substring);
 		System.out.println(substring.length());
--- src/main/java/com/howtodoinjava/demo/SpringBootActuatorExampleApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootActuatorExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootActuatorExampleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootActuatorExampleApplication.class, args);
 	}
 
